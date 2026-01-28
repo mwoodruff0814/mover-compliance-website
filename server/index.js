45421f791f4b66fa97af57cc829fd35d0eec8f3d
Matt Woodruff <matt@worryfreemovers.com>
2026-01-28 18:58:16 -0500

    Remove incompatible 404 handler for Express 5.x

--- server/index.js
+++ server/index.js
@@ -143,14 +143,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// 404 handler for API routes
-app.use('/api/*', (req, res) => {
-  res.status(404).json({
-    success: false,
-    message: 'API endpoint not found'
-  });
-});
-
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
